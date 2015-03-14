e84a189e427f24cfcfc2d484f120639e0b4e9dd7
aksivaram2k2 <aksivaram2k2@gmail.com>
2015-03-14 19:16:05 +0530

    changes

--- src/JavaExamples/HttpWebRequestExample.java
+++
@@ -1,9 +0,0 @@
-//http://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
-package JavaExamples;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
